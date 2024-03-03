5a2315d597127791fd33c74b25f9f1f5dcf8826e
Mikael <mikaelhjfb@gmail.com>
2024-03-03 13:55:33 -0300

    chore: initialize laravel project with docker

---
+++ portal-noticias-api/app/Services/ArticleService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Services;
+use App\Models\Article;
+use App\DTO\Article\ArticleIndexDTO;
+use App\DTO\Article\ArticleStoreDTO;
+use App\DTO\Article\ArticleUpdateDTO;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+use App\Http\Resources\ArticleIndexResource;
+
+
+class ArticleService
+{
+    public function index(int $perPage = 10): LengthAwarePaginator
+    {
+        $articles = Article::orderBy("published_at", "desc")->paginate($perPage);
+        return $articles;
+    }
+
+    public function store(ArticleStoreDTO $articleStoreDTO): Article
+    {
+        $article = Article::create([
+            'title' => $articleStoreDTO->title,
+            'description' => $articleStoreDTO->description,
+            'content' => $articleStoreDTO->content,
+            'image' => $articleStoreDTO->image,
+            'published_at' => $articleStoreDTO->published_at
+        ]);
+
+        return $article;
+    }
+
+    public function show(int $id): Article
+    {
+        $article = Article::find($id);
+        return $article;
+    }
+
+    public function update(ArticleUpdateDTO $articleUpdateDTO, Article $article): Article
+    {
+        $article->update([
+            'title' => $articleUpdateDTO->title,
+            'description' => $articleUpdateDTO->description,
+            'content' => $articleUpdateDTO->content,
+            'image' => $articleUpdateDTO->image,
+            'published_at' => $articleUpdateDTO->published_at
+        ]);
+
+        return $article;
+    }
+
+    public function destroy(Article $article): Article
+    {
+        $article->delete();
+        return $article;
+    }
+
+}
