814918083bbd977aa8ebb618cd858bfe3fbda2cd
Mikael <mikaelhjfb@gmail.com>
2024-03-03 19:08:07 -0300

    fix: fix routes

--- portal-noticias-front/src/router/index.ts
+++ portal-noticias-front/src/router/index.ts
@@ -1,23 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AppInicio from '../views/AppInicio.vue'
+import AppMateria from '../views/AppMateria.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'materias',
+      component: AppInicio
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/materia/:id',
+      name: 'materia',
+      component: AppMateria,
+      props: true
     }
   ]
 })
 
 export default router
+
